ae956accd334ebbc01ad26d0c59663af78f7af48
PaulCoinmanlabs <coinmanlabs@gmail.com>
2026-01-21 22:25:28 +0800

    4.进行代币之间转账有税相关

--- contracts/test/UltimateBSCCoin.ts
+++ contracts/test/UltimateBSCCoin.ts
@@ -1,10 +1,12 @@
 import {
   time,
   loadFixture,
+  setBalance,
 } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre, { ethers } from "hardhat";
+import { impersonateAccount } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 
 describe("UltimateBSCCoin Test", function () {
   async function deployTokenFixture() {
@@ -61,13 +63,51 @@ describe("UltimateBSCCoin Test", function () {
       await token.transfer(user1.address, ethers.parseEther("1000"));
       await token.enableTrading();
 
-      await token.connect(user1).transfer(user2.address, ethers.parseEther("100"));
+      await token
+        .connect(user1)
+        .transfer(user2.address, ethers.parseEther("100"));
 
       expect(await token.balanceOf(user1.address)).to.be.equal(
         ethers.parseEther("900"),
       );
       expect(await token.balanceOf(user2.address)).to.be.equal(
         ethers.parseEther("100"),
+      );
+    });
+  });
+
+  describe("3. 税收逻辑测试", function () {
+    it("白名单转账没有税", async function () {
+      const { user1, token } = await loadFixture(deployTokenFixture);
+      await token.enableTrading();
+      await token.transfer(user1.address, ethers.parseEther("100"));
+
+      expect(await token.balanceOf(user1.address)).to.be.equal(
+        ethers.parseEther("100"),
+      );
+    });
+
+    it("模拟买入（从池子到用户），应该扣除5%的买入税", async function () {
+      const { user1, token } = await loadFixture(deployTokenFixture);
+      const pairAddress = await token.pair();
+      await token.enableTrading()
+      // 接管这个账号 因为pair是合约的地址 我们是获取不到私钥 user owner是hardhat提供的 知道私钥
+      await impersonateAccount(pairAddress);
+      // 获取pair的签名者
+      const pairSigner = await ethers.getSigner(pairAddress);
+
+      // 添加池子
+      await token.transfer(pairAddress, ethers.parseEther("10000"));
+      // 因为发起转账不仅需要有token 还需要有gas 所以借助setBalance给该账户设置gas
+      await setBalance(pairAddress, ethers.parseEther("100"));
+
+      // 模拟pair向user1转账就是买入了
+      await token
+        .connect(pairSigner)
+        .transfer(user1.address, ethers.parseEther("100"));
+
+      expect(await token.balanceOf(user1.address)).to.be.equal(
+        ethers.parseEther("95"),
       );
     });
   });
