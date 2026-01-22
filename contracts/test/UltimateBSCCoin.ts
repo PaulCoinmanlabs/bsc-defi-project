22bea41fe15168f54e47cba3e2ec2b103254ebee
PaulCoinmanlabs <coinmanlabs@gmail.com>
2026-01-22 10:14:18 +0800

    5.合约相关的测试全部完成通过

--- contracts/test/UltimateBSCCoin.ts
+++ contracts/test/UltimateBSCCoin.ts
@@ -55,7 +55,7 @@ describe("UltimateBSCCoin Test", function () {
       await token.transfer(user1.address, ethers.parseEther("1000"));
       await expect(
         token.connect(user1).transfer(user2.address, ethers.parseEther("100")),
-      ).to.be.revertedWith("Trading is not active");
+      ).to.be.revertedWith("Trading is not active.");
     });
 
     it("开启交易后 普通用户可以转账", async function () {
@@ -90,7 +90,7 @@ describe("UltimateBSCCoin Test", function () {
     it("模拟买入（从池子到用户），应该扣除5%的买入税", async function () {
       const { user1, token } = await loadFixture(deployTokenFixture);
       const pairAddress = await token.pair();
-      await token.enableTrading()
+      await token.enableTrading();
       // 接管这个账号 因为pair是合约的地址 我们是获取不到私钥 user owner是hardhat提供的 知道私钥
       await impersonateAccount(pairAddress);
       // 获取pair的签名者
@@ -109,6 +109,65 @@ describe("UltimateBSCCoin Test", function () {
       expect(await token.balanceOf(user1.address)).to.be.equal(
         ethers.parseEther("95"),
       );
+    });
+
+    it("模拟卖出（从用户到池子），应该扣除10%的税", async function () {
+      const { token, user1 } = await loadFixture(deployTokenFixture);
+
+      await token.enableTrading();
+
+      await token.transfer(user1.address, ethers.parseEther("1000"));
+
+      const pairAddress = await token.pair();
+
+      await token
+        .connect(user1)
+        .transfer(pairAddress, ethers.parseEther("1000"));
+
+      expect(await token.balanceOf(pairAddress)).to.be.equal(
+        ethers.parseEther("900"),
+      );
+    });
+  });
+
+  describe("4.限制和黑名单测试", function () {
+    it("每个钱包最大持仓限制, 模拟从池子里面买入", async function () {
+      const { token, user1 } = await loadFixture(deployTokenFixture);
+
+      // 往池子里面注入资金
+      const pairAddress = await token.pair();
+
+      await impersonateAccount(pairAddress);
+
+      const pairSigner = await ethers.getSigner(pairAddress);
+
+      await token.transfer(pairAddress, ethers.parseEther("100000000"));
+      await setBalance(pairAddress, ethers.parseEther("100"));
+
+      await token.enableTrading();
+
+      const maxWallet = await token.maxWalletAmount();
+
+      await expect(
+        token
+          .connect(pairSigner)
+          .transfer(user1.address, maxWallet + ethers.parseEther("1")),
+      ).to.be.revertedWith("Buy amount exceeds maxTxAmount");
+    });
+
+    it("黑名单测试", async function () {
+      const { token, user1, user2 } = await loadFixture(deployTokenFixture);
+      await token.enableTrading();
+      await token.transfer(user1.address, ethers.parseEther("100"));
+
+      // 将user1设置为黑名单
+      await token.setBlacklist(user1.address, true);
+
+      // 因为user1被设置为了黑名单 所以无法转账等操作
+
+      await expect(
+        token.connect(user1).transfer(user2.address, ethers.parseEther("10"))
+      ).to.be.revertedWith("Address is blacklisted");
     });
   });
 });
